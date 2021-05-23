05ead29ed5886b73a5d92bf359eb1d567d24e427
SanghwaLee <devlsh0124@gmail.com>
2021-05-23 23:59:27 +0900

    feat: Add creat, join room

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var server = require("http").createServer(app);
 // http server를 socket.io server로 upgrade한다
 var io = require("socket.io")(server);
 
+const room_info = {};
+
+const makeRandomName = () => {
+  var name = "";
+  var possible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (var i = 0; i < 5; i++) {
+    name += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return name;
+};
+
 // React 연동 시 필요 없음
 // localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
 app.get("/", function (req, res) {
@@ -11,26 +22,84 @@ app.get("/", function (req, res) {
 
 // connection event handler
 // connection이 수립되면 event handler function의 인자로 socket인 들어온다
-io.on("connection", function (socket) {
+io.on("connection", (socket) => {
   console.log("Socket ID : " + socket.id);
 
-  // 접속한 클라이언트의 정보가 수신되면
-  socket.on("login", function (data) {
-    console.log(
-      "Client logged-in:\n name:" + data.name + "\n room: " + data.room
-    );
+  // 접속한 클라이언트의 정보 수신
+  socket.on("login", (data) => {
+    if (data.type === "create" || data.type === "join") {
+      console.log(
+        "Client logged-in:\n name:" +
+          data.name +
+          "\n room: " +
+          data.room +
+          "\n type: " +
+          data.type
+      );
 
-    // socket에 클라이언트 정보를 저장한다
-    socket.name = data.name;
-    socket.room = data.room;
+      var room_name = makeRandomName();
 
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    io.emit("login", data.name);
+      // 클라이언트 정보 소켓에 저장
+      socket.name = data.name;
+    }
+
+    // 클라이언트의 접속 방식을 확인하여 처리
+    // 방 생성의 경우
+    if (data.type === "create") {
+      // 새로운 방 정보 생성
+      room_info[room_name] = {
+        player: [
+          {
+            id: socket.id,
+            name: data.name,
+          },
+        ],
+        ready: [false, false],
+      };
+
+      // 방 이름 소켓에 저장
+      socket.room = room_name;
+
+      // 클라이언트에 현재 방 상태 전달
+      socket.emit("create", room_name);
+    }
+    // 방 참여의 경우
+    else if (data.type === "join") {
+      console.log(data.room in room_info);
+      // 기존 방 존재 여부 확인
+      if (!(data.room in room_info)) {
+        console.log(socket.id + " access Wrong Room");
+        return;
+      }
+
+      // 기존 방에 사용자 추가
+      room_name = data.room;
+      room_info[room_name].player.push({
+        id: socket.id,
+        name: data.name,
+      });
+
+      // 방 이름 소켓에 저장
+      socket.room = room_name;
+
+      // 현재 클라이언트에게 정보 전달
+      socket.emit("join", room_info[room_name].player[0]);
+
+      // 상대방에게 클라이언트 정보 전달
+      io.to(room_info[room_name].player[0].id).emit("newPlayer", {
+        id: socket.id,
+        name: data.name,
+      });
+    }
+    console.log("room info");
+    console.log(room_info);
   });
+
+  //
 
   // 클라이언트로부터의 메시지가 수신되면
   socket.on("chat", function (data) {
-    console.log("Message from %s: %s", socket.name, data.msg);
+    console.log("Message from %s: %s : %s", socket.name, data.msg, data.type);
 
     var msg = {
       from: {
@@ -64,6 +133,6 @@ io.on("connection", function (socket) {
   });
 });
 
-server.listen(3000, function () {
+server.listen(3001, function () {
   console.log("Socket IO server listening on port 3000");
 });
